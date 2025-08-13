8650d3aba7eb4b4d4d6a4883a8808f5b7a8fd284
MagmaDevil <naver201010@gmail.com>
2025-08-13 21:41:27 +0900

    [2025.8.13] 완전탐색

---
+++ Class3th (Bubble Sort)/Source.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 거품 정렬
+
+    //서로 인접한 두 원소를 비교하여 정렬하는 알고리즘
+
+    double list[] = { 5,3,2,1,4,6,8,10,9,11,15,13,14,12 };
+
+    int size = sizeof(list) / sizeof(list[0]);
+
+    for (int j = 0; j < size - 1; j++)
+    {
+        for (int i = 0; i < (size - j) - 1; i++)
+        {
+            if (list[i] > list[i + 1])
+            {
+                swap(list[i], list[i + 1]);
+            }
+        }
+    }
+
+    for (const auto& element : list)
+    {
+        cout << element << " ";
+    }
+
+#pragma endregion
+
+
+    return 0;
+}
---
+++ Class4th (Select Sort)/Source.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 선택 정렬
+
+    //주어진 리스트 중에 최솟값을 찾은 다음 특정한 위치에서 결과를 교체
+
+    int list[] = { 8,5,4,3,0,1,6,7,2,9 };
+
+    int size = sizeof(list) / sizeof(list[0]);
+
+    for (int j = 0; j < size = 1; j++)
+    {
+        int min = list[j];
+
+        int index = j;
+
+        for (int i = j + 1; i < size; i++)
+        {
+            if (min > list[i])
+            {
+                min = list[i];
+
+                index = i;
+            }
+        }
+
+        swap(list[j], list[index]);
+    }
+
+    for (const auto& element : list)
+    {
+        cout << element << " ";
+    }
+
+#pragma endregion
+
+
+    return 0;
+}
