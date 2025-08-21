de91e5891898744bde9469325683dce1585a9a61
MagmaDevil <naver201010@gmail.com>
2025-08-21 21:44:02 +0900

    [2025.8.21] 이분탐색

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,9 +2,70 @@
 
 using namespace std;
 
+void sort(int list[], int start, int end)
+{
+	if(start >= end)
+	{
+		return;
+	}
+
+	int pivot = start;
+	int left = start + 1;
+	int right = end;
+
+	int half = end / 2;
+
+	while (left <= right)
+	{
+		while (left <= end && list[pivot] >= list[left])
+		{
+			left++;
+		}
+
+		while (right > start && list[pivot] <= list[right])
+		{
+			right--;
+		}
+
+		if(left > right)
+		{
+			swap(list[pivot], list[right]);
+		}
+
+		else
+		{
+			swap(list[left], list[right]);
+		}
+	}
+
+	sort(list, start, right - 1);
+	sort(list, right + 1, end);
+}
+
 int main()
 {
-	float x = 45;
+#pragma region 퀵 정렬
+
+	// 기준점을 획득한 다음 기준점을 기준으로 배열을 나누고 한 쪽에는
+	// 기준점보다 작은 값들이 위치하게 한 다음 다른 한 쪽에는 기준점보다
+	// 큰 값들이 위치하도록 정렬합니다.
+	
+	// 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
+	// 모든 배열이 기본 배열이 될 때까지 반복하면 정렬합니다.
+
+	int list[] = {5,3,8,4,1,6,2,7};
+
+	int size = sizeof(list) / sizeof(list[0]);
+
+	sort(list, 0, size - 1);
+
+	for (const auto& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
 
 	return 0;
 }
